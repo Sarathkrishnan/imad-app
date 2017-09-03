5e7fcd981c241ec670f25ca03db29d6c2fd4b3e9
Sarathkrishnan <sarathkrishnangs@gmail.com>
2017-09-02 20:18:41 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,15 +10,15 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one',function(req,res) {
-   res.send("Articleone will be send"); 
+   res.send("Article one will be send"); 
 });
 
 app.get('/article-two',function(req,res) {
-   res.send("Articleone will be send"); 
+   res.send("Article two will be send"); 
 });
 
 app.get('/article-three',function(req,res) {
-   res.send("Articleone will be send"); 
+   res.send("Article three will be send"); 
 });
 
 app.get('/ui/style.css', function (req, res) {
