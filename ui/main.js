8340cbbb21acb219d10fb424dea6dfd584359f3c
Sarathkrishnan <sarathkrishnangs@gmail.com>
2017-09-04 21:05:12 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,9 @@
 console.log('Loaded!');
 
 var element = document.getElementById('mytext');
-
 element.innerHTML = "Hai I am on the jouney ..";
+
+var img = document.getElementById('image');
+img.onclick=function() {
+    img.style.marginleft = '100px';
+}
