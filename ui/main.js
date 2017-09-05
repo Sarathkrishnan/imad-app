bbff4c3cb4f45f02e923d7e989bbf62f28b021ee
Sarathkrishnan <sarathkrishnangs@gmail.com>
2017-09-04 21:29:04 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -10,5 +10,5 @@ function moveRight() {
     img.style.marginLeft=marginLeft + 'px';
 }
 img.onclick=function() {
-    var intervel = setInetrval(moveRight,50);
+    var intervel = setInetrval(moveRight,100);
 };
